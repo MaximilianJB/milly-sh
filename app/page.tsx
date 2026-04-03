efac46bd2a7bbb3e5660ba31f69e9ffd68f2f825
Maximilian <brownmaxj@gmail.com>
2026-04-02 23:12:44 -0500

    initial commit 2 ( the real one )

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col items-center justify-center h-screen bg-white dark:bg-black">
+        <h1 className="font-pixel-line font-bold text-6xl">milly.sh</h1>
+        <h3 className="font-pixel-line font-bold text-xl text-amber-500">
+          work in progress
+        </h3>
+      </main>
+    </div>
+  );
+}
